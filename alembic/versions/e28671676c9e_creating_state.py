c0501daf0f1a6d384a0551aa1aa7b983ec2b7546
Ni1cky <nikmbogdanov@gmail.com>
2023-05-20 14:31:21 +0300

    Migration with state added

---
+++ alembic/versions/e28671676c9e_creating_state.py
@@ -0,0 +1,24 @@
+"""Creating state
+
+Revision ID: e28671676c9e
+Revises: 
+Create Date: 2023-05-20 13:20:02.941572
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e28671676c9e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
