2063979787bb8431120c4a8c31efcdf46f0d9f0b
Ni1cky <nikmbogdanov@gmail.com>
2023-05-20 14:30:52 +0300

    PEP8 fixed

--- store/config.py
+++ store/config.py
@@ -5,7 +5,7 @@ class PostgresConfig(BaseSettings):
     db: str
     password: str = 'postgres'
     server: str = '127.0.0.1'
-    user: str  = 'postgres'
+    user: str = 'postgres'
     port: int = '5433'
 
     class Config:
